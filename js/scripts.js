356ae1584d7918779f724799d54ac8aaf0ef8743
Tien Le <its.just.tien@gmail.com>
2019-01-18 14:00:55 -0800

    finish conditional, add content

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,35 @@
 $(document).ready(function() {
-  $(".partTime").hide();
-  $(".frontCss").hide();
-  $(".backRuby").hide();
-  $(".backJava").hide();
+  // $(".partTime").hide();
+  // $(".frontCss").hide();
+  // $(".backRuby").hide();
+  // $(".backJava").hide();
 
-  $("#trackForm").submit(function(event) {
-    event.preventDefault();
-
+  // $("#trackForm").submit(function(event) {
+  //   event.preventDefault();
+    var name = $("#name").val();
     var fpAvail = $("input:radio[name=partFull]:checked").val();
     var fbFocus = $("#fbFocus").val();
     var awType = $("#awType").val();
 
+    if (name) {
       if (fpAvail === "ftime") {
         if (fbFocus === "Back End") {
           if(awType === "andr") {
             $(".backJava").show();
+
           } else {
               $(".backRuby").show();
               $(".backJava").hide();
-
           }
-        // else  {}
-          }
+        } else  {
+          $(".frontCss").show();
         }
-      })
-  })
+      } else {
+        $(".partTime").show();
+        }
+      } else {
+        alert("Please enter your name!!")
+    }
+
+    });
+  });
